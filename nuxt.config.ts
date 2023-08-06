ebd79e069967244c8cbc76a2b0944a407280450e
ハン・キョンス <hankyonsu@hankyonsunoMacBook-Air.local>
2023-08-06 23:10:27 +0900

    nuxt3の編集

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+export default {
+  target: 'static',
+  generate: {subFolders: true},
+  router: {
+    base: '/nuxt3-link-test/'
+  }
+}
